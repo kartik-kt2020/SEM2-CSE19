cc644fa66b40dd92b9af70205c5db6aa64d7caf8
kartik <kartikkumar240922@gmail.com>
2026-06-19 01:20:39 +0530

    exp8

---
+++ vite-project/src/experiment8.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import "./index.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
+  return (
+    <div className="container">
+      <div className="counter-card">
+        <h1>React Counter Application</h1>
+
+        <h2>{count}</h2>
+
+        <div className="buttons">
+          <button onClick={increment}>
+            Increment (+)
+          </button>
+
+          <button onClick={decrement}>
+            Decrement (-)
+          </button>
+        </div>
+
+        <button onClick={reset}>
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
